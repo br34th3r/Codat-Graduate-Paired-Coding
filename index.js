78388ed724a93481ea9e1f8b3a377ea29b1990c0
Joshua Boddy <joshuaboddy1@gmail.com>
2022-07-13 12:21:03 +0100

    Added some brief data validation

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
+const Validator = require('jsonschema').Validator;
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
 const PORT = 3000;
+
+const v = new Validator();
+
+Validator.prototype.customFormats.notEmptyString = function(input) {
+    return input.length;
+}
+
+const invoice_schema = {
+    "id": "/Invoice",
+    "type": "object",
+    "properties": {
+        "invoiceNumber": {"type": "string", "format": "notEmptyString"},
+        "customerName": {"type": "string", "format": "notEmptyString"},
+        "amount": {"type": "number"},
+        "payments": {
+            "type": "array",
+            "items": {"$ref": "/Payment"}
+        }
+    },
+    "required": ["invoiceNumber", "customerName", "amount", "payments"]
+}
+
+const payment_schema = {
+    "id": "/Payment",
+    "type": "object",
+    "properties": {
+        "paymentNumber": {"type": "string", "format": "notEmptyString"},
+        "amount": {"type": "number"}
+    },
+    "required": ["paymentNumber", "amount"]
+}
+
+v.addSchema(payment_schema, '/Payment');
+v.addSchema(invoice_schema, '/Invoice');
 
 var invoices = [{
     "invoiceNumber": "1",
@@ -77,9 +112,16 @@ function convertDictToOutput() {
 
 // Payments comes through as an array
 app.post("/sendInvoice", jsonParser, (req, res) => {
-    invoices.push(req.body);
-    res.status(200);
-    res.send("Success");
+    const isValidInvoice = v.validate(req.body, invoice_schema, { allowUnknownAttributes: false }).valid;
+    console.log(isValidInvoice);
+    if (isValidInvoice) {
+        invoices.push(req.body);
+        res.status(200);
+        res.send("Success");
+    } else {
+        res.status(400);
+        res.send("Invoice did not pass validation!");
+    }
 });
 
 app.get("/getReports", (req, res) => {
